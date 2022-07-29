11cb9eddf66b8d79497d431f672a89433d13e6c6
unknown <olena.denysova88@gmail.com>
2022-07-29 17:52:39 +0200

    product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,11 +4,12 @@ interface ProductProps {
     product: IProduct;
 }
 
-export default function Product(props: ProductProps) {
-    
+export default function Product({ product }: ProductProps) {
     return (
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-            Product
+            <img src={product.image} className="w-1/6" alt={product.title} />
+            <p>{product.title}</p>
+            <p className="font-bold">{product.price}</p>
         </div>
     );
 }
